3239b016d87dc2b6ed89831018dca79380082408
u <fjrfhdy@gmail.com>
2026-05-29 02:04:37 -0600

    basic version of the calculator(cant be used now)

---
+++ basic_calculations.py
@@ -0,0 +1,37 @@
+def add(y):
+    e = y.split(" ")
+    t =[]
+    for i in e:
+        a = int(i)
+        t.append(a)
+    return sum(t)
+
+def substitution(y):
+    e = y.split(" ")
+    t =[]
+    for i in e:
+        a = int(i)
+        t.append(a)
+    r =0
+    for i in range(0,len(t)):
+        if i ==0:
+           r = t[0]
+           continue
+        r -= t[i]
+    return r
+print(substitution("1 2 5"))          
+
+def multiplying(y):
+    e = y.split(" ")
+    t =[]
+    for i in e:
+        a = int(i)
+        t.append(a)
+    r =0
+    for i in range(0,len(t)):
+        if i ==0:
+           r = t[0]
+           continue
+        r *= t[i]
+    return r
+print(multiplying("2 5"))          
---
+++ calc.py
@@ -0,0 +1,9 @@
+print("""
+1- subtraction
+2- add
+3- dividing
+4- multiplying
+""")
+
+user_input = input("\nchoose by number:").strip()
+
